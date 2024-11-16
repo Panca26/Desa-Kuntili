14208b7a06b6126ce6a640853a48c9f697e2c3a2
Panca26 <pipoy0883@gmail.com>
2024-11-16 23:40:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Handle Hamburger Menu
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
